ab8e39485f3f4ae58eee35bf0e0eafa982d38ad8
93b7f1 <Maftycolors@gmail.com>
2023-11-03 03:00:55 -0300

    vibrato app v2

--- Vibrato/app/src/main/java/com/example/vibrato/EchoFlow.kt
+++ Vibrato/app/src/main/java/com/example/vibrato/EchoFlow.kt
@@ -24,6 +24,9 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.io.IOException
+import okhttp3.*
+
+
 
 class EchoFlow : AppCompatActivity() {
     private var echoId: Int = 0
@@ -32,7 +35,10 @@ class EchoFlow : AppCompatActivity() {
     private lateinit var imageEcho: ImageView
     private var imageTranslationY: Float = 0f
     private var mediaPlayer: MediaPlayer? = null
-
+    private var currentPreviewUrl: String? = null
+    private lateinit var textView: TextView
+    private lateinit var artImage: ImageView
+    private lateinit var artistName: TextView
     fun imageViewClicked(view: View) {
         val authManager = AuthManager(this)
 
@@ -50,6 +56,9 @@ class EchoFlow : AppCompatActivity() {
         echoId = intent.getIntExtra("echoId", 0)
 
         imageEcho = findViewById(R.id.imageEcho)
+        textView = findViewById(R.id.echoname)
+        artImage = findViewById(R.id.artmagem)
+        artistName = findViewById(R.id.artistname)
 
         imageEcho.setOnTouchListener { view, event ->
             when (event.action) {
@@ -63,9 +72,8 @@ class EchoFlow : AppCompatActivity() {
                     val newY = event.y
                     val deltaY = newY - previousY
 
-                    // Atualize a posição vertical da imagem
                     imageEcho.translationY += deltaY
-                    imageTranslationY += deltaY // Atualize a variável de controle
+                    imageTranslationY += deltaY
 
                     previousY = newY
                     true
@@ -77,13 +85,11 @@ class EchoFlow : AppCompatActivity() {
                     val minDeltaY = 200
 
                     if (deltaY < -minDeltaY) {
-                        // Atualize os valores e o echoId
                         echoId++
                         updateEchoValues(echoId)
                     } else {
-                        // Animação para mover a imagem de volta para a posição de origem
                         imageEcho.animate().translationY(0f).setDuration(500).start()
-                        imageTranslationY = 0f // Redefina a variável de controle
+                        imageTranslationY = 0f
                     }
                     true
                 }
@@ -93,12 +99,69 @@ class EchoFlow : AppCompatActivity() {
         }
 
         updateEchoValues(echoId)
+
     }
 
-    private fun fetchSpotifyData() {
+    private fun updateEchoValues(newEchoId: Int) {
+        val retrofitClient = Conexao.getRetrofitInstance("/echo")
+        val endpoint = retrofitClient.create(Endpoint::class.java)
+
+        imageEcho.translationY = 0f
+        imageTranslationY = 0f
+
+        endpoint.listarEchoes().enqueue(object : Callback<List<Map<String, Any>>?> {
+            override fun onFailure(call: Call<List<Map<String, Any>>?>, t: Throwable) {
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(
+                call: Call<List<Map<String, Any>>?>,
+                response: Response<List<Map<String, Any>>?>
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        val gson = Gson()
+                        val echoesList = mutableListOf<Echo>()
+
+                        for (echoMap in responseBody) {
+                            val echoJson = gson.toJson(echoMap)
+                            val echo = gson.fromJson(echoJson, Echo::class.java)
+                            echoesList.add(echo)
+                        }
+
+                        val echoIndex = newEchoId % echoesList.size
+                        val selectedEcho = echoesList[echoIndex]
+
+                        textView.text = selectedEcho.tituloMusica
+                        artistName.text = selectedEcho.artista.nome
+
+                        val imageUrl = "https://vibratosimages.blob.core.windows.net/imagens/${selectedEcho.blob}"
+                        Glide.with(this@EchoFlow)
+                            .load(imageUrl)
+                            .centerCrop()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(imageEcho)
+
+                        val imageUrl2 = "https://vibratosimages.blob.core.windows.net/imagens/${selectedEcho.artista.blob}"
+                        Glide.with(this@EchoFlow)
+                            .load(imageUrl2)
+                            .centerCrop()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(artImage)
+                        fetchSpotifyData(selectedEcho.otherLink1)
+                    }
+
+                } else {
+                    Toast.makeText(baseContext, "Erro na resposta", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+
+    private fun fetchSpotifyData(trackId: String) {
         val clientId = "654f22146f75469296716116ef4fe9d9"
         val clientSecret = "4356007d93504cdbbb54d48861567992"
-        val trackId = "58ijfpSyfBjA5TK79fa914" // ID da faixa específica
 
         val client = OkHttpClient()
         val requestBody = FormBody.Builder()
@@ -123,7 +186,6 @@ class EchoFlow : AppCompatActivity() {
                     val json = response.body()?.string()
                     val token = JSONObject(json).getString("access_token")
 
-                    // Agora que você tem o token, pode fazer a chamada GET para obter informações da faixa
                     val trackUrl = "https://api.spotify.com/v1/tracks/$trackId"
                     val trackRequest = Request.Builder()
                         .url(trackUrl)
@@ -141,6 +203,9 @@ class EchoFlow : AppCompatActivity() {
                                 val trackObject = JSONObject(trackJson)
 
                                 val previewUrl = trackObject.getString("preview_url")
+
+                                mediaPlayer?.release()
+                                mediaPlayer = null
 
                                 mediaPlayer = MediaPlayer()
                                 mediaPlayer?.setAudioAttributes(
@@ -162,141 +227,59 @@ class EchoFlow : AppCompatActivity() {
                                     mediaPlayer?.seekTo(0)
                                     mediaPlayer?.start()
                                 }
+
+                                currentPreviewUrl = previewUrl
                             }
                         }
                     })
                 }
-            }
-        })
-    }
-
-    private fun updateEchoValues(newEchoId: Int) {
-        val retrofitClient = Conexao.getRetrofitInstance("/echo")
-        val endpoint = retrofitClient.create(Endpoint::class.java)
-        val callback = endpoint.listarEchoes()
-        val textView: TextView = findViewById(R.id.echoname)
-        val imageEchoes: ImageView = findViewById(R.id.imageEcho)
-        val ArtistImage: ImageView = findViewById(R.id.artmagem)
-        val artname: TextView = findViewById(R.id.artistname)
-        imageEcho.translationY = 0f
-        imageTranslationY = 0f
-        callback.enqueue(object : Callback<List<Map<String, Any>>> {
-            override fun onFailure(call: Call<List<Map<String, Any>>>, t: Throwable) {
-                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onResponse(
-                call: Call<List<Map<String, Any>>>,
-                response: Response<List<Map<String, Any>>>
-            ) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        val gson = Gson()
-                        val echoesList = mutableListOf<Echo>()
-
-                        for (echoMap in responseBody) {
-                            val echoJson = gson.toJson(echoMap)
-                            val echo = gson.fromJson(echoJson, Echo::class.java)
-                            echoesList.add(echo)
+                imageEcho.setOnTouchListener { view, event ->
+                    when (event.action) {
+                        MotionEvent.ACTION_DOWN -> {
+                            initialY = event.y
+                            previousY = initialY
+                            true
                         }
 
-                        val echoIndex = newEchoId % echoesList.size
-                        val selectedEcho = echoesList[echoIndex]
-                        val idEcho = selectedEcho.idEcho
-                        val artista = selectedEcho.artista
-                        val tituloMusica = selectedEcho.tituloMusica
-                        val artistaImageee = selectedEcho.artista.blob
-                        val namee = selectedEcho.artista.nome
-                        val images = selectedEcho.blob
-                        textView.text = tituloMusica
-                        artname.text = namee
+                        MotionEvent.ACTION_MOVE -> {
+                            val newY = event.y
+                            val deltaY = newY - previousY
 
-                        val imageUrl = "https://vibratosimages.blob.core.windows.net/imagens/$images"
-                        Glide.with(this@EchoFlow)
-                            .load(imageUrl)
-                            .centerCrop()
-                            .diskCacheStrategy(DiskCacheStrategy.ALL)
-                            .into(imageEchoes)
+                            imageEcho.translationY += deltaY
+                            imageTranslationY += deltaY
 
-                        val imageUrl2 = "https://vibratosimages.blob.core.windows.net/imagens/$artistaImageee"
-                        Glide.with(this@EchoFlow)
-                            .load(imageUrl2)
-                            .centerCrop()
-                            .diskCacheStrategy(DiskCacheStrategy.ALL)
-                            .into(ArtistImage)
-                    }
-                } else {
-                    Toast.makeText(baseContext, "Erro na resposta", Toast.LENGTH_SHORT).show()
-                }
-            }
-        })
-    }
-    private fun updateEchoValues2(newEchoId: Int) {
-        val retrofitClient = Conexao.getRetrofitInstance("/echo")
-        val endpoint = retrofitClient.create(Endpoint::class.java)
-        val callback = endpoint.listarEchoes()
-        val textView: TextView = findViewById(R.id.echoname)
-        val imageEchoes: ImageView = findViewById(R.id.imageEcho)
-        val ArtistImage: ImageView = findViewById(R.id.artmagem)
-        val artname: TextView = findViewById(R.id.artistname)
-        imageEcho.translationY = 0f
-        imageTranslationY = 0f
-        callback.enqueue(object : Callback<List<Map<String, Any>>> {
-            override fun onFailure(call: Call<List<Map<String, Any>>>, t: Throwable) {
-                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onResponse(
-                call: Call<List<Map<String, Any>>>,
-                response: Response<List<Map<String, Any>>>
-            ) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        val gson = Gson()
-                        val echoesList = mutableListOf<Echo>()
-
-                        for (echoMap in responseBody) {
-                            val echoJson = gson.toJson(echoMap)
-                            val echo = gson.fromJson(echoJson, Echo::class.java)
-                            echoesList.add(echo)
+                            previousY = newY
+                            true
                         }
 
-                        val echoIndex = newEchoId % echoesList.size
-                        val selectedEcho = echoesList[echoIndex]
-                        val idEcho = selectedEcho.idEcho
-                        val artista = selectedEcho.artista
-                        val tituloMusica = selectedEcho.tituloMusica
-                        val artistaImageee = selectedEcho.artista.blob
-                        val namee = selectedEcho.artista.nome
-                        val images = selectedEcho.blob
-                        textView.text = tituloMusica
-                        artname.text = namee
+                        MotionEvent.ACTION_UP -> {
+                            val finalY = event.y
+                            val deltaY = finalY - initialY
+                            val minDeltaY = 200
 
-                        val imageUrl = "https://vibratosimages.blob.core.windows.net/imagens/$images"
-                        Glide.with(this@EchoFlow)
-                            .load(imageUrl)
-                            .centerCrop()
-                            .diskCacheStrategy(DiskCacheStrategy.ALL)
-                            .into(imageEchoes)
+                            if (deltaY < -minDeltaY) {
+                                echoId++
+                                updateEchoValues(echoId)
+                            } else {
+                                imageEcho.animate().translationY(0f).setDuration(500).start()
+                                imageTranslationY = 0f
 
-                        val imageUrl2 = "https://vibratosimages.blob.core.windows.net/imagens/$artistaImageee"
-                        Glide.with(this@EchoFlow)
-                            .load(imageUrl2)
-                            .centerCrop()
-                            .diskCacheStrategy(DiskCacheStrategy.ALL)
-                            .into(ArtistImage)
+                                if (mediaPlayer?.isPlaying == true) {
+                                    mediaPlayer?.pause()
+                                } else {
+                                    mediaPlayer?.start()
+                                }
+                            }
+                            true
+                        }
+
+                        else -> false
                     }
-                } else {
-                    Toast.makeText(baseContext, "Erro na resposta", Toast.LENGTH_SHORT).show()
                 }
             }
         })
     }
 }
-
-
 
 
 
